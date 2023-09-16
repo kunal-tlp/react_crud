d88531f494de0825cc88f6abf13d7ddc6c45dc85
kunalgiri <kunalgiri0123@gmail.com>
2023-09-16 21:08:02 +0530

    :

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Create from './components/Create';
+import Read from './components/Read';
+import Update from './components/Update';
 
 function App() {
   return (
-    <div className="App">
-      <Create /> 
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Create />}></Route>
+          <Route path='/read' element={<Read />}></Route>
+          <Route path='/update' element={<Update />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,29 +1,48 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Create = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const history = useNavigate();
+
+    const header = { "Access-Control-Allow-Origin": "*" };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log("Submit Successfully..");
+        axios.post('https://6501ed63736d26322f5c7f5a.mockapi.io/react-crud', {
+            name: name,
+            email: email,
+            header,
+        })
+            .then(() => {
+                history('/read');
+            });
+    };
     return <>
-        <div className='container'>
+        <div className='d-flex justify-content-between m-2'>
             <h2>Create</h2>
-            <form>
-                <div className="mb-3">
-                    <label className="form-label">Name</label>
-                    <input 
-                        type="name" 
-                        className="form-control"
-                        onChange={(e) =>setName(e.target.value)}  />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Email address</label>
-                    <input 
-                        type="email" 
-                        className="form-control"
-                        onChange={(e) =>setEmail(e.target.value)} />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            <Link to="/read"><button className='btn btn-primary'>Show Data</button></Link>
         </div>
+        <form>
+            <div className="mb-3">
+                <label className="form-label">Name</label>
+                <input
+                    type="name"
+                    className="form-control"
+                    onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div className="mb-3">
+                <label className="form-label">Email address</label>
+                <input
+                    type="email"
+                    className="form-control"
+                    onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+        </form>
     </>
 }
 
